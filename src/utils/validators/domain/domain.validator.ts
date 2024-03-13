9e0b874557562d60b88cd5b59870dcb187557aaa
dev-Raffa <mnraffaell@gmail.com>
2024-03-13 09:38:11 -0300

    refact(validator): refact domain validator

--- src/utils/validators/domain/domain.validator.ts
+++ src/utils/validators/domain/domain.validator.ts
@@ -1,15 +1,18 @@
+import { IValidator } from 'src/utils/interfaces/validator/validator.interface';
 import { enumTLD } from '../../../utils/enums/tld/tld.enum';
 import { DomainPattern } from './pattern/domain.pattern';
 
-export function DomainValidator(domain: string) {
-  domain.toLowerCase();
-  const domainParts = DomainPattern.exec(domain);
+export class DomainValidator implements IValidator {
+  validate(arg: any): boolean {
+    arg.toLowerCase();
+    const domainParts = DomainPattern.exec(arg);
 
-  if (!domainParts) return false;
+    if (!domainParts) return false;
 
-  const tld = domainParts[2].split('.');
+    const tld = domainParts[2].split('.');
 
-  return tld.length > 2
-    ? tld[tld.length - 1] in enumTLD && tld[tld.length - 2] in enumTLD
-    : tld[tld.length - 1] in enumTLD;
+    return tld.length > 2
+      ? tld[tld.length - 1] in enumTLD && tld[tld.length - 2] in enumTLD
+      : tld[tld.length - 1] in enumTLD;
+  }
 }
