fc35fd427c6ec60d191c5e14c8cc81207f37753e
dev-Raffa <mnraffaell@gmail.com>
2024-03-13 09:39:51 -0300

    refact(validator): refact email validator

--- src/utils/validators/email/email.validator.ts
+++ src/utils/validators/email/email.validator.ts
@@ -1,17 +1,20 @@
+import { IValidator } from 'src/utils/interfaces/validator/validator.interface';
 import { enumTLD } from '../../../utils/enums/tld/tld.enum';
 import { EmailPattern } from './pattern/email.patern';
 
-export function EmailValidator(email: string): boolean {
-  email.toLowerCase();
-  const emailParts = EmailPattern.exec(email);
+export class EmailValidator implements IValidator {
+  validate(email: string): boolean {
+    email.toLowerCase();
+    const emailParts = EmailPattern.exec(email);
 
-  if (!emailParts) {
-    return false;
+    if (!emailParts) {
+      return false;
+    }
+
+    const tld = emailParts[3].split('.');
+
+    return tld.length > 2
+      ? tld[tld.length - 1] in enumTLD && tld[tld.length - 2] in enumTLD
+      : tld[tld.length - 1] in enumTLD;
   }
-
-  const tld = emailParts[3].split('.');
-
-  return tld.length > 2
-    ? tld[tld.length - 1] in enumTLD && tld[tld.length - 2] in enumTLD
-    : tld[tld.length - 1] in enumTLD;
 }
