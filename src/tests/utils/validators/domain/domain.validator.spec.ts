cc07289ca729e264f04b512c7ced01acf0561e43
dev-Raffa <mnraffaell@gmail.com>
2024-03-14 10:37:56 -0300

    test(domain-validator): refact test domain validator

--- src/tests/utils/validators/domain/domain.validator.spec.ts
+++ src/tests/utils/validators/domain/domain.validator.spec.ts
@@ -1,10 +1,11 @@
 import { DomainValidator } from '../../../../utils/validators/domain/domain.validator';
 
 describe('domain validator', () => {
+  const validator = new DomainValidator();
   it('should return false when domain format is invalid', () => {
-    const testDomain_A = DomainValidator('company@company.com');
-    const testDomain_B = DomainValidator('company_com');
-    const testDomain_C = DomainValidator('.company.com');
+    const testDomain_A = validator.validate('company@company.com');
+    const testDomain_B = validator.validate('company_com');
+    const testDomain_C = validator.validate('.company.com');
 
     expect(testDomain_A).toBe(false);
     expect(testDomain_B).toBe(false);
@@ -12,14 +13,14 @@ describe('domain validator', () => {
   });
 
   it('should return false when top-level domain is invalid', () => {
-    const testDomain = DomainValidator('company.xyz.com');
+    const testDomain = validator.validate('company.xyz.com');
 
     expect(testDomain).toBe(false);
   });
 
   it('should true when domain format and tld are valid', () => {
-    const testDomain_A = DomainValidator('company.com.br');
-    const testDomain_B = DomainValidator('company.com');
+    const testDomain_A = validator.validate('company.com.br');
+    const testDomain_B = validator.validate('company.com');
 
     expect(testDomain_A).toBe(true);
     expect(testDomain_B).toBe(true);
