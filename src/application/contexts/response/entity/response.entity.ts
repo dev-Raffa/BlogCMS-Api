1592e3c33b4492d47ba507b03c97effdf710efa7
dev-Raffa <mnraffaell@gmail.com>
2024-03-14 10:35:22 -0300

    refact(response): refact response entity

--- src/application/contexts/response/entity/response.entity.ts
+++ src/application/contexts/response/entity/response.entity.ts
@@ -2,6 +2,7 @@ import { IUser } from '../../user/interfaces/entity/user-entity.interface';
 import { IResponse } from '../interfaces/entity/response-entity.interface';
 
 export class ResponseEntity implements IResponse {
+  id: number;
   author: IUser;
   text: string;
   likes: number;
