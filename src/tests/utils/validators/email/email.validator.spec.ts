36f2e5d02e167586837e01c50bebee3f6b9a2cd5
dev-Raffa <mnraffaell@gmail.com>
2024-03-14 10:39:49 -0300

    test(email-validator): refact test email validator

--- src/tests/utils/validators/email/email.validator.spec.ts
+++ src/tests/utils/validators/email/email.validator.spec.ts
@@ -1,11 +1,13 @@
 import { EmailValidator } from '../../../../utils/validators/email/email.validator';
 
 describe('email validator', () => {
+  const validator = new EmailValidator();
+
   it('should return false when email format is not valid', () => {
-    const testEmail_A = EmailValidator('juninho.com');
-    const testEmail_B = EmailValidator('junho@@company.com');
-    const testEmail_C = EmailValidator('junho@company.');
-    const testEmail_D = EmailValidator('@company.com');
+    const testEmail_A = validator.validate('juninho.com');
+    const testEmail_B = validator.validate('junho@@company.com');
+    const testEmail_C = validator.validate('junho@company.');
+    const testEmail_D = validator.validate('@company.com');
 
     expect(testEmail_A).toBe(false);
     expect(testEmail_B).toBe(false);
@@ -14,13 +16,13 @@ describe('email validator', () => {
   });
 
   it('should return false when top-level domain is invalid', () => {
-    const testEmail = EmailValidator('juninho@company.teste.com');
+    const testEmail = validator.validate('juninho@company.teste.com');
 
     expect(testEmail).toBe(false);
   });
 
   it('should return true when email format and top-level domain are valid', () => {
-    const testEmail = EmailValidator('juninho@company.com');
+    const testEmail = validator.validate('juninho@company.com');
 
     expect(testEmail).toBe(true);
   });
