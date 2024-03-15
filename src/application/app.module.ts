239e118114850547a1a7d636fb791483411d7891
dev-Raffa <mnraffaell@gmail.com>
2024-03-15 17:58:24 -0300

    refact(app module): upgrade modules importeds

--- src/application/app.module.ts
+++ src/application/app.module.ts
@@ -2,8 +2,28 @@ import { Module } from '@nestjs/common';
 import { ClientModule } from './contexts/client/client.module';
 import { UserModule } from './contexts/user/user.module';
 import { LevelModule } from './contexts/level/level.module';
+import { AuthModule } from './contexts/auth/auth.module';
+import { RolesCommmentsModule } from './contexts/roles/comments/rolesComments.module';
+import { RolesPostsModule } from './contexts/roles/posts/rolesPost.module';
+import { RolesUsersModule } from './contexts/roles/users/rolesUsers.module';
+import { PostModule } from './contexts/post/post.module';
+import { CommentModule } from './contexts/comment/comment.module';
+import { ResponseModule } from './contexts/response/response.module';
+import { SubscriberModule } from './contexts/subscriber/subscriber.module';
 
 @Module({
-  imports: [ClientModule, UserModule, LevelModule]
+  imports: [
+    AuthModule,
+    ClientModule,
+    UserModule,
+    LevelModule,
+    RolesUsersModule,
+    RolesPostsModule,
+    RolesCommmentsModule,
+    PostModule,
+    CommentModule,
+    ResponseModule,
+    SubscriberModule
+  ]
 })
 export class AppModule {}
