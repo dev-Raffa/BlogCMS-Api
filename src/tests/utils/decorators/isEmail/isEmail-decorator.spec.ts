f863a728ec8cc2366e5250952999a6553f8bf19e
Rafael da Conceição <mnraffaell@gmail.com>
2024-03-24 08:18:58 -0300

    feat(decorators): add isEmail decorator

--- src/tests/utils/decorators/isEmail/isEmail-decorator.spec.ts
+++ src/tests/utils/decorators/isEmail/isEmail-decorator.spec.ts
@@ -2,11 +2,11 @@ import { EmailValidator } from '../../../../utils/validators/email/email.validat
 import { Validate } from '../../../../utils/decorators/Validate/validate.decorator';
 import { isEmail } from '../../../../utils/decorators/isEmail/isEmail.decorator';
 
-describe.skip('isEmail decorator', () => {
-  jest.mock('../../../../utils/decorators/validate/validate.decorator', () => ({
-    Validate: jest.fn()
-  }));
+jest.mock('../../../../utils/decorators/validate/validate.decorator', () => ({
+  Validate: jest.fn()
+}));
 
+describe('isEmail decorator', () => {
   it('should call Validate decorator with EmailValidator', () => {
     const emailDecorator = isEmail();
     emailDecorator;
